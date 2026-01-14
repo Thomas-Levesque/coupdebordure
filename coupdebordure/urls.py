b7bbacba63160a9dc33abdee8d30fca21364964e
thomas levesque <thomaslevesque@MacBook-Pro.local>
2026-01-14 14:07:28 +0100

    Fix indentation error in urls.py

--- coupdebordure/urls.py
+++ coupdebordure/urls.py
@@ -6,6 +6,8 @@ from django.conf.urls.i18n import i18n_patterns
 from django.urls import include, path
 from django.urls import path, include
 from accounts import views as accounts_views
+from django.urls import path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -18,6 +20,25 @@ urlpatterns = [
     path("", include("stats.urls")),
     path("i18n/", include("django.conf.urls.i18n")),
     path("i18n/set-language-pro/", accounts_views.set_language_pro, name="set_language_pro"),
+    path("password-reset/", auth_views.PasswordResetView.as_view(
+        template_name="accounts/password_reset_form.html",
+        email_template_name="accounts/password_reset_email.txt",
+        subject_template_name="accounts/password_reset_subject.txt",
+        success_url="/password-reset/done/",
+    ), name="password_reset"),
+
+    path("password-reset/done/", auth_views.PasswordResetDoneView.as_view(
+        template_name="accounts/password_reset_done.html",
+    ), name="password_reset_done"),
+
+    path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(
+        template_name="accounts/password_reset_confirm.html",
+        success_url="/reset/done/",
+    ), name="password_reset_confirm"),
+
+    path("reset/done/", auth_views.PasswordResetCompleteView.as_view(
+        template_name="accounts/password_reset_complete.html",
+    ), name="password_reset_complete"),
 
 ]
 
