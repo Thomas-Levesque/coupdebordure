3a1e880ea65b9d7bd5ce04a969541b7665e4a9a0
thomas levesque <thomaslevesque@MacBook-Pro.local>
2026-01-17 14:42:38 +0100

    Configure SMTP Brevo for transactional emails

--- coupdebordure/settings.py
+++ coupdebordure/settings.py
@@ -160,9 +160,14 @@ if not DEBUG:
     SECURE_REFERRER_POLICY = "same-origin"
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp-relay.brevo.com")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", "587"))
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "true").lower() == "true"
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "Coup de Bordure <no-reply@coupdebordure.fr>")
+
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
+
+DEFAULT_FROM_EMAIL = os.getenv(
+    "DEFAULT_FROM_EMAIL",
+    "Coup de Bordure <no-reply@coupdebordure.com>"
+)
