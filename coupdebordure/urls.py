a866bd5875e0dfb5b5b92bcc8ed3e03399029372
thomas levesque <thomaslevesque@MacBook-Pro.local>
2026-01-18 13:06:25 +0100

    Fix password reset template (i18n) and URLs

--- coupdebordure/urls.py
+++ coupdebordure/urls.py
@@ -1,25 +1,25 @@
-from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.i18n import i18n_patterns
-from django.urls import include, path
-from django.urls import path, include
-from accounts import views as accounts_views
-from django.urls import path
 from django.contrib.auth import views as auth_views
+from accounts import views as accounts_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
     path("", include("pages.urls")),
     path("", include("accounts.urls")),
     path("", include("races.urls")),
     path("", include("bets.urls")),
     path("", include("leagues.urls")),
-    path("push/", include("pushes.urls")),
     path("", include("stats.urls")),
+
+    path("push/", include("pushes.urls")),
+
     path("i18n/", include("django.conf.urls.i18n")),
     path("i18n/set-language-pro/", accounts_views.set_language_pro, name="set_language_pro"),
+
     path("password-reset/", auth_views.PasswordResetView.as_view(
         template_name="accounts/password_reset_form.html",
         email_template_name="accounts/password_reset_email.txt",
@@ -39,7 +39,6 @@ urlpatterns = [
     path("reset/done/", auth_views.PasswordResetCompleteView.as_view(
         template_name="accounts/password_reset_complete.html",
     ), name="password_reset_complete"),
-
 ]
 
 if settings.DEBUG:
